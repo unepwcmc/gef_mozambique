80c617ba204953a02593492ef9f7d001db6175f5
nym1k <sleeping.stars@gmail.com>
2021-07-29 12:07:49 +0100

    Initial statistics grid block

---
+++ content/themes/gef-mozambique/blocks/statistics-grid/block.php
@@ -0,0 +1,24 @@
+<?php
+  /*
+    Statistics Grid Block
+    Created by UNEP-WCMC
+    With Block Lab for Gutenberg - https://getblocklab.com/
+  */
+?>
+
+<div class="stats-grid">
+  <ul class="stats-grid__items">
+    <?php for ($i=0; $i < 8; $i++): ?>
+      <li class="stats-grid__item">
+        <div class="stats-tile">
+          <h4 class="stats-tile__heading">
+            0000
+          </h4>
+          <h5 class="stats-tile__subheading">
+            Total number of species
+          </h5>
+        </div>
+      </li>
+    <?php endfor; ?>
+  </ul>
+</div>
