7c0037341af602d776a524d0156537d1c15ecf4e
nym1k <sleeping.stars@gmail.com>
2021-07-30 11:30:14 +0100

    Futher styles for statistics grid, gutenberg styles

--- content/themes/gef-mozambique/blocks/statistics-grid/block.php
+++ content/themes/gef-mozambique/blocks/statistics-grid/block.php
@@ -6,19 +6,25 @@
   */
 ?>
 
-<div class="stats-grid">
-  <ul class="stats-grid__items">
-    <?php for ($i=0; $i < 8; $i++): ?>
-      <li class="stats-grid__item">
-        <div class="stats-tile">
-          <h4 class="stats-tile__heading">
-            0000
-          </h4>
-          <h5 class="stats-tile__subheading">
-            Total number of species
-          </h5>
-        </div>
-      </li>
-    <?php endfor; ?>
-  </ul>
-</div>
+<?php if ( block_rows( 'statistic' ) ) : ?>
+  <div class="stats-grid">
+    <ul class="stats-grid__items">
+      <?php while ( block_rows( 'statistic' ) ) : block_row( 'statistic' );?>
+        <li class="stats-grid__item">
+          <div class="stats-tile">
+            <?php if ( block_sub_value( 'figure' ) ) : ?>
+              <h4 class="stats-tile__heading">
+                <?php echo block_sub_value( 'figure' ); ?>
+              </h4>
+            <?php endif;?>
+            <?php if ( block_sub_value( 'text' ) ) : ?>
+              <h5 class="stats-tile__subheading">
+                <?php echo block_sub_value( 'text' ); ?>
+              </h5>
+            <?php endif;?>
+          </div>
+        </li>
+      <?php endwhile; ?>
+    </ul>
+  </div>
+<?php endif;?>
