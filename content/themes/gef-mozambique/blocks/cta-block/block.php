c255fe3c3ca20b5dad19736f03aeb4f0b0e99ed6
nym1k <sleeping.stars@gmail.com>
2021-08-03 12:35:37 +0100

    CTA block

--- content/themes/gef-mozambique/blocks/cta-block/block.php
+++ content/themes/gef-mozambique/blocks/cta-block/block.php
@@ -6,46 +6,113 @@
   */
 
   // Variables
-
-  $title = get_theme_mod( 'cta_block_title' );
-  $text = get_theme_mod( 'cta_block_text' );
-  $link_url = get_theme_mod( 'cta_block_button_link' );
-  $link_url_external = get_theme_mod( 'cta_block_button_link_external' );
-  $link_text = get_theme_mod( 'cta_block_button_text' );
-  $background_image_url = get_theme_mod( 'cta_block_background_image' );
-  $background_image_opacity = (get_theme_mod( 'cta_block_background_image_opacity' ) * 0.1);
-  $background_colour = get_theme_mod( 'cta_block_background_colour' );
+  $image = block_field( 'background-image', false );
+  $image_url = wp_get_attachment_image_src( $image, 'full-size' )[0];
+  $image_alt = get_post_meta($image, '_wp_attachment_image_alt', true);
 ?>
 
-<div
-  class="cta-block"
-  style="background-color: <?php echo $background_colour; ?>;"
->
-  <img
-    src="<?php echo $background_image_url; ?>"
-    alt="<?php echo $title; ?>"
-    class="cta-block__background-image"
-    <?php if ($background_image_opacity != 1) echo 'style="opacity: ' . $background_image_opacity . ';"'; ?>
-  >
-  <div class="cta-block__inner">
-    <div class="cta-block__body">
-      <div class="cta-block__content<?php if (!$background_image_url) echo ' cta-block__content--no-image'; ?>">
-        <h3 class="cta-block__title">
-          <?php echo $title; ?>
-        </h3>
-        <p class="cta-block__text">
-          <?php echo $text; ?>
-        </p>
-        <a
-          href="<?php echo $link_url; ?>"
-          class="cta-block__link<?php if ($link_url_external) echo ' cta-block__link--external'; ?>"
-          <?php if ($link_url_external) echo ' target="_blank"'; ?>
-          <?php echo 'title="' . $link_text . '"'; ?>
-        >
-          <?php echo $link_text; ?>
-        </a>
+<div class="block-cta">
+  <div class="block-cta__inner">
+    <div class="block-cta__body">
+      <div class="block-cta__content">
+
+        <?php if ( block_rows( 'featured-links' ) ) : ?>
+
+          <div class="block-cta__featured">
+            <ul class="block-cta__featured-items">
+
+              <?php while ( block_rows( 'featured-links' ) ) : block_row( 'featured-links' );?>
+
+                <li class="block-cta__featured-item">
+                  <div class="block-cta__featured-tile">
+
+                    <?php if ( block_sub_value( 'title' ) ) : ?>
+                      <h3 class="block-cta__title">
+                        <?php block_sub_field( 'title' ); ?>
+                      </h3>
+                    <?php endif; ?>
+
+                    <?php if ( block_sub_value( 'description' ) ) : ?>
+                      <p class="block-cta__text">
+                        <?php block_sub_field( 'description' ); ?>
+                      </p>
+                    <?php endif; ?>
+
+                    <?php if ( block_sub_value( 'link-url' ) ) : ?>
+                      <?php
+                        $link_url = block_sub_value( 'link-url' );
+                        $link_text = block_sub_value( 'link-text' );
+                        $external_link = block_sub_value( 'external-link' );
+                      ?>
+
+                      <p
+                        class="block-cta__link<?php if ($external_link) echo ' block-cta__link--external'; ?>"
+                      >
+                        <?php echo $link_text; ?>
+                        <?php if ($external_link) get_template_part( 'template-parts/icons/icon', 'external' ); ?>
+                      </p>
+
+                      <a
+                        href="<?php echo $link_url; ?>"
+                        <?php echo 'title="' . block_sub_value( 'link-text' ) . '"'; ?>
+                        <?php if ($external_link) echo 'target="_blank"'; ?>
+                        class="block-cta__fauxlink"
+                      >
+                        <?php echo $link_text; ?>
+                      </a>
+                    <?php endif; ?>
+
+                  </div>
+                </li>
+
+              <?php endwhile; wp_reset_query(); ?>
+
+            </ul>
+          </div>
+        <?php endif; ?>
       </div>
-      <div class="cta-block__bottom-border"></div>
+      <?php if ( block_rows( 'other-links' ) ) : ?>
+
+        <aside class="block-cta__list">
+          <h3 class="block-cta__list-title">
+            <?php block_field('other-links-title'); ?>
+          </h3>
+
+          <ul class="block-cta__list-items">
+
+            <?php while ( block_rows( 'other-links' ) ) : block_row( 'other-links' );?>
+
+              <?php if ( block_sub_value( 'link-url' ) ) : ?>
+                <?php
+                  $link_url = block_sub_value( 'link-url' );
+                  $link_text = block_sub_value( 'link-text' );
+                  $external_link = block_sub_value( 'external-link' );
+                ?>
+
+                <li class="block-cta__list-item">
+                  <a
+                    href="<?php echo $link_url; ?>"
+                    title="<?php echo $link_text; ?>"
+                    <?php if ($external_link) echo 'target="_blank"'; ?>
+                    class="block-cta__list-link<?php if ($external_link) echo ' block-cta__list-link--external'; ?>"
+                  >
+                    <?php echo $link_text; ?>
+                    <?php if ($external_link) get_template_part( 'template-parts/icons/icon', 'external' ); ?>
+                  </a>
+                </li>
+              <?php endif; ?>
+
+            <?php endwhile; wp_reset_query(); ?>
+
+          </ul>
+        </aside>
+
+      <?php endif; ?>
+      <img
+        src="<?php echo $image_url; ?>"
+        alt="<?php echo $image_alt; ?>"
+        class="block-cta__background-image"
+      >
     </div>
   </div>
 </div>
--- content/themes/gef-mozambique/template-parts/icons/icon-external.php
+++ content/themes/gef-mozambique/template-parts/icons/icon-external.php
@@ -1,6 +1,13 @@
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 11.282 11.282">
-  <g data-name="Group 25" opacity=".7" fill="none" stroke="currentColor" stroke-linecap="round" stroke-miterlimit="10" stroke-width="1.5">
-    <path data-name="Path 449" d="M2.59.75h7.943v7.943" stroke-linejoin="round"/>
-    <path data-name="Line 25" d="M1.061 10.221l9-9"/>
-  </g>
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  viewBox="0 0 194.818 194.818"
+>
+  <path
+    d="M185.818 2.161h-57.04c-4.971 0-9 4.029-9 9s4.029 9 9 9h35.312l-86.3 86.3a9 9 0 006.364 15.364 8.975 8.975 0 006.364-2.636l86.3-86.3v35.313c0 4.971 4.029 9 9 9s9-4.029 9-9v-57.04a9 9 0 00-9-9.001z"
+    fill="currentColor"
+  />
+  <path
+    d="M149 77.201a9 9 0 00-9 9v88.456H18v-122h93.778c4.971 0 9-4.029 9-9s-4.029-9-9-9H9a9 9 0 00-9 9v140a9 9 0 009 9h140a9 9 0 009-9V86.201a9 9 0 00-9-9z"
+    fill="currentColor"
+  />
 </svg>
