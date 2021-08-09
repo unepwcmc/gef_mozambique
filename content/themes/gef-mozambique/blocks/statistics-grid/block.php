1dfafef9b5814cda7502c83febcc23f85b571713
nym1k <sleeping.stars@gmail.com>
2021-08-09 15:14:41 +0100

    Adding inner to stats grid in widgets

--- content/themes/gef-mozambique/blocks/statistics-grid/block.php
+++ content/themes/gef-mozambique/blocks/statistics-grid/block.php
@@ -8,23 +8,25 @@
 
 <?php if ( block_rows( 'statistic' ) ) : ?>
   <div class="stats-grid">
-    <ul class="stats-grid__items">
-      <?php while ( block_rows( 'statistic' ) ) : block_row( 'statistic' );?>
-        <li class="stats-grid__item">
-          <div class="stats-tile">
-            <?php if ( block_sub_value( 'figure' ) ) : ?>
-              <h4 class="stats-tile__heading">
-                <?php echo block_sub_value( 'figure' ); ?>
-              </h4>
-            <?php endif;?>
-            <?php if ( block_sub_value( 'text' ) ) : ?>
-              <h5 class="stats-tile__subheading">
-                <?php echo block_sub_value( 'text' ); ?>
-              </h5>
-            <?php endif;?>
-          </div>
-        </li>
-      <?php endwhile; ?>
-    </ul>
+    <div class="stats-grid_inner">
+      <ul class="stats-grid__items">
+        <?php while ( block_rows( 'statistic' ) ) : block_row( 'statistic' );?>
+          <li class="stats-grid__item">
+            <div class="stats-tile">
+              <?php if ( block_sub_value( 'figure' ) ) : ?>
+                <h4 class="stats-tile__heading">
+                  <?php echo block_sub_value( 'figure' ); ?>
+                </h4>
+              <?php endif;?>
+              <?php if ( block_sub_value( 'text' ) ) : ?>
+                <h5 class="stats-tile__subheading">
+                  <?php echo block_sub_value( 'text' ); ?>
+                </h5>
+              <?php endif;?>
+            </div>
+          </li>
+        <?php endwhile; ?>
+      </ul>
+    </div>
   </div>
 <?php endif;?>
