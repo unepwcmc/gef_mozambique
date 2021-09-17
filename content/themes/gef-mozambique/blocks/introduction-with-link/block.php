9ea265ab1ecf6356ea34d0d8104c6bbcf69cf65f
nym1k <sleeping.stars@gmail.com>
2021-09-17 13:44:42 +0100

    Intro block responsive style

--- content/themes/gef-mozambique/blocks/introduction-with-link/block.php
+++ content/themes/gef-mozambique/blocks/introduction-with-link/block.php
@@ -23,79 +23,76 @@
 ?>
 
 <div class="block-introduction">
-  <div class="block-introduction__inner">
+  <div class="block-introduction__body">
+    <div class="block-introduction__columns">
 
-    <div class="block-introduction__body">
+        <div class="block-introduction__column">
+          <div class="block-introduction__content">
 
-      <div class="block-introduction__columns">
+            <?php if ( $text ) : ?>
+              <div class="block-introduction__text">
+                <?php echo $text; ?>
+              </div>
+            <?php endif;?>
 
-          <div class="block-introduction__column">
-            <div class="block-introduction__content">
+            <?php if ($link_url && $link_text): ?>
+              <a
+                href="<?php echo $link_url; ?>"
+                title="<?php echo $link_text; ?>"
+                <?php if ($external_link) echo ' target="_blank"'; ?>
+                class="block-introduction__link<?php if ($external_link) echo ' block-introduction__link--external'; ?>"
+              >
+                <?php echo $link_text; ?>
+                <?php if ($external_link) get_template_part( 'template-parts/icons/icon', 'external' ); ?>
+              </a>
+            <?php endif; ?>
 
-              <?php if ( $text ) : ?>
-                <div class="block-introduction__text">
-                  <?php echo $text; ?>
-                </div>
-              <?php endif;?>
+            <?php if ($modal_downloads): ?>
+              <modal-downloads
+                text='<?php echo htmlentities($modal_text); ?>'
+                :downloads='<?php echo json_encode($modal_downloads); ?>'
+              ></modal-downloads>
+            <?php endif; ?>
 
-              <?php if ($link_url && $link_text): ?>
-                <a
-                  href="<?php echo $link_url; ?>"
-                  title="<?php echo $link_text; ?>"
-                  <?php if ($external_link) echo ' target="_blank"'; ?>
-                  class="block-introduction__link<?php if ($external_link) echo ' block-introduction__link--external'; ?>"
-                >
-                  <?php echo $link_text; ?>
-                  <?php if ($external_link) get_template_part( 'template-parts/icons/icon', 'external' ); ?>
-                </a>
-              <?php endif; ?>
-
-              <?php if ($modal_downloads): ?>
-                <modal-downloads
-                  text='<?php echo htmlentities($modal_text); ?>'
-                  :downloads='<?php echo json_encode($modal_downloads); ?>'
-                ></modal-downloads>
-              <?php endif; ?>
-
-            </div>
           </div>
+        </div>
 
-          <div class="block-introduction__column">
-            <div class="block-introduction__featured-link">
-              <?php if ( $featured_logo ) : ?>
-                <img
-                  src="<?php echo $featured_logo_url; ?>"
-                  alt="<?php echo $featured_heading; ?>"
-                  class="block-introduction__logo"
-                />
-              <?php endif;?>
-              <?php if ( $featured_heading ) : ?>
-                <h4 class="block-introduction__heading">
-                  <?php echo $featured_heading; ?>
-                </h4>
-              <?php endif;?>
+        <div class="block-introduction__column">
+          <div class="block-introduction__featured-link">
+            <?php if ( $featured_logo ) : ?>
+              <img
+                src="<?php echo $featured_logo_url; ?>"
+                alt="<?php echo $featured_heading; ?>"
+                class="block-introduction__logo"
+              />
+            <?php endif;?>
+            <?php if ( $featured_heading ) : ?>
+              <h4 class="block-introduction__heading">
+                <?php echo $featured_heading; ?>
+              </h4>
+            <?php endif;?>
 
-              <?php if ( $featured_description ) : ?>
-                <p class="block-introduction__description">
-                  <?php echo $featured_description; ?>
-                </p>
-              <?php endif;?>
+            <?php if ( $featured_description ) : ?>
+              <p class="block-introduction__description">
+                <?php echo $featured_description; ?>
+              </p>
+            <?php endif;?>
 
-              <?php if ($featured_link_url && $featured_link_text): ?>
-                <a
-                  href="<?php echo $featured_link_url; ?>"
-                  title="<?php echo $featured_link_text; ?>"
-                  <?php if ($featured_external_link) echo ' target="_blank"'; ?>
-                  class="block-introduction__link block-introduction__link--text<?php if ($featured_external_link) echo ' block-introduction__link--external'; ?>"
-                >
-                  <?php echo $featured_link_text; ?>
-                  <?php if ($featured_external_link) get_template_part( 'template-parts/icons/icon', 'external' ); ?>
-                </a>
-              <?php endif; ?>
-            </div>
+            <?php if ($featured_link_url && $featured_link_text): ?>
+              <a
+                href="<?php echo $featured_link_url; ?>"
+                title="<?php echo $featured_link_text; ?>"
+                <?php if ($featured_external_link) echo ' target="_blank"'; ?>
+                class="block-introduction__link block-introduction__link--text<?php if ($featured_external_link) echo ' block-introduction__link--external'; ?>"
+              >
+                <?php echo $featured_link_text; ?>
+                <?php if ($featured_external_link) get_template_part( 'template-parts/icons/icon', 'external' ); ?>
+              </a>
+            <?php endif; ?>
+
           </div>
+        </div>
 
-      </div>
     </div>
   </div>
 </div>
