df4ad72be734ad0e0edd7f77dbcef3b98e569a73
nym1k <sleeping.stars@gmail.com>
2021-09-22 14:28:03 +0100

    Vue translations

--- content/themes/gef-mozambique/translations/wcmc-vue.php
+++ content/themes/gef-mozambique/translations/wcmc-vue.php
@@ -3,49 +3,27 @@
 return array(
   'common' => array(
     'download' => __( 'Download', 'wcmc-vue' ),
-    'explore' => __( 'Explore', 'wcmc-vue' ),
-    'filter' => __( 'Filter', 'wcmc-vue' ),
-    'filters' => __( 'Filters', 'wcmc-vue' ),
-    'filter_toggle' => __( 'Filter Toggle', 'wcmc-vue' ),
-    'more' => __( 'More', 'wcmc-vue' ),
-    'no_results_found' => __( 'No results found.', 'wcmc-vue' ),
-    'related_documents' => __( 'Related Documents', 'wcmc-vue' ),
-    'view_all' => __( 'View all', 'wcmc-vue')
+    'explore' => __( 'Explorar', 'wcmc-vue' ),
+    'filter' => __( 'Filtro', 'wcmc-vue' ),
+    'filters' => __( 'Filtros', 'wcmc-vue' ),
+    'filter_toggle' => __( 'Filtro Alternar', 'wcmc-vue' ),
+    'more' => __( 'Mais', 'wcmc-vue' ),
+    'no_results_found' => __( 'Nenhum resultado encontrado.', 'wcmc-vue' ),
+    'related_documents' => __( 'Documentos Relacionados', 'wcmc-vue' ),
+    'view_all' => __( 'Ver tudo', 'wcmc-vue')
   ),
   'listing' => array(
-    'count_text' => __( '<span>Showing </span>%{posts_length} of %{max_posts}<span> entries</span>', 'wcmc-vue' ),
+    'count_text' => __( '<span>Mostrando </span>%{posts_length} de %{max_posts}<span> entradas</span>', 'wcmc-vue' ),
   ),
   'post_types' => array(
-    'event' => __( 'event', 'wcmc-vue' ),
-    'events' => __( 'events', 'wcmc-vue' ),
-    'news' => __( 'news', 'wcmc-vue' ),
-    'multimedia' => __( 'multimedia', 'wcmc-vue' ),
-    'resource' => __( 'resource', 'wcmc-vue' ),
-    'resources' => __( 'resources', 'wcmc-vue' ),
-    'partner' => __( 'partner', 'wcmc-vue' ),
-    'partners' => __( 'partners', 'wcmc-vue' ),
+    'event' => __( 'evento', 'wcmc-vue' ),
+    'events' => __( 'eventos', 'wcmc-vue' ),
+    'news' => __( 'notícia', 'wcmc-vue' ),
+    'multimedia' => __( 'multimídia', 'wcmc-vue' ),
+    'resource' => __( 'recurso', 'wcmc-vue' ),
+    'resources' => __( 'recursos', 'wcmc-vue' ),
     'post' => __( 'post', 'wcmc-vue' ),
     'posts' => __( 'posts', 'wcmc-vue' ),
-  ),
-  'resources' => array(
-    'case_study' => __( 'Case Study', 'wcmc-vue' ),
-    'case_studies' => __( 'Case Studies', 'wcmc-vue' ),
-    'guide' => __( 'Guide', 'wcmc-vue' ),
-    'guides' => __( 'Guides', 'wcmc-vue' ),
-    'report' => __( 'Report', 'wcmc-vue' ),
-    'reports' => __( 'Reports', 'wcmc-vue' ),
-    'resource' => __( 'Resource', 'wcmc-vue' ),
-    'resources' => __( 'Resources', 'wcmc-vue' ),
-    'webinar' => __( 'Webinar', 'wcmc-vue' ),
-    'webinars' => __( 'Webinars', 'wcmc-vue' )
-  ),
-  'search' => array(
-    'search' => __( 'Search', 'wcmc-vue' ),
-    'search_for' => __( 'Search for:', 'wcmc-vue' ),
-    'search_here' => __( 'Search here', 'wcmc-vue' )
-  ),
-  'social' => array(
-    'twitter' => __( 'Twitter', 'wcmc-vue' )
   ),
   'table' => array(
     'jump_to_database' => __( 'Jump To Database', 'wcmc-vue' )
