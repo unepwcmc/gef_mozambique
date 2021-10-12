67e34246c76b9b5e044adf3b410f900bd9ac4290
nym1k <sleeping.stars@gmail.com>
2021-10-12 08:31:22 +0100

    Add menu text to menu trigger

--- content/themes/gef-mozambique/template-parts/header/tools.php
+++ content/themes/gef-mozambique/template-parts/header/tools.php
@@ -1,29 +1,10 @@
 <div class="header-tools">
   <div class="header-tools__items">
-    <?php /*
-      <div class="header-tools__item header-tools__item--search">
-        <header-search form-action="<?php echo esc_url( home_url( '/' ) ); ?>" />
-      </div>
-    */ ?>
-
     <?php if ( get_theme_mod( 'enable_language_switcher' ) ) : ?>
       <div class="header-tools__item header-tools__item--language-switcher">
         <?php do_action( 'wpml_add_language_selector' ); ?>
       </div>
     <?php endif; ?>
-
-    <?php /*
-      <?php
-        if (get_theme_mod( 'enable_header_button' )):
-          $external_header_link = get_theme_mod( 'header_button_external_link' );
-      ?>
-        <div class="header-tools__item header-tools__item--cta">
-          <a href="<?php echo get_theme_mod( 'header_button_url' ); ?>" class="header-tools__button<?php if ($external_header_link) echo ' header-tools__button--external'; ?>" <?php if ($external_header_link) echo 'target="_blank"' ?> title="<?php echo get_theme_mod( 'header_button_text' ); ?>">
-            <?php echo get_theme_mod( 'header_button_text' ); ?><?php if ($external_header_link) get_template_part( 'template-parts/icons/icon', 'external' ); ?>
-          </a>
-        </div>
-      <?php endif; ?>
-    */ ?>
 
     <div class="header-tools__item header-tools__item--menu-toggle">
       <drawer-trigger
@@ -32,6 +13,7 @@
         drawer="menu"
         label="<?php _e( 'Menu Toggle', 'wcmc' ); ?>"
       >
+        <?php _e( 'Menu', 'wcmc' ); ?>
         <?php get_template_part( 'template-parts/icons/icon', 'menu' ); ?>
       </drawer-trigger>
     </div>
