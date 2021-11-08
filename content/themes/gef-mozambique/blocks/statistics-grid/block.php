bff42b04c2bea6a38b26726822f09398e9267380
nym1k <sleeping.stars@gmail.com>
2021-11-08 11:30:10 +0000

    feat: gutenberg styles for stats grid block

--- content/themes/gef-mozambique/blocks/statistics-grid/block.php
+++ content/themes/gef-mozambique/blocks/statistics-grid/block.php
@@ -13,6 +13,16 @@
         <?php while ( block_rows( 'statistic' ) ) : block_row( 'statistic' );?>
           <li class="stats-grid__item">
             <div class="stats-tile">
+              <?php if ( block_sub_value( 'figure' ) ) : ?>
+                <h4 class="stats-tile__heading">
+                  <?php echo block_sub_value( 'figure' ); ?>
+                </h4>
+              <?php endif;?>
+              <?php if ( block_sub_value( 'text' ) ) : ?>
+                <h5 class="stats-tile__subheading">
+                  <?php echo block_sub_value( 'text' ); ?>
+                </h5>
+              <?php endif;?>
               <?php if ( block_sub_value( 'tooltip' ) ) : ?>
                 <div class="stats-tile__tooltip">
                   <div class="stats-tile__tooltip-icon">
@@ -24,16 +34,6 @@
                     </span>
                   </div>
                 </div>
-              <?php endif;?>
-              <?php if ( block_sub_value( 'figure' ) ) : ?>
-                <h4 class="stats-tile__heading">
-                  <?php echo block_sub_value( 'figure' ); ?>
-                </h4>
-              <?php endif;?>
-              <?php if ( block_sub_value( 'text' ) ) : ?>
-                <h5 class="stats-tile__subheading">
-                  <?php echo block_sub_value( 'text' ); ?>
-                </h5>
               <?php endif;?>
             </div>
           </li>
