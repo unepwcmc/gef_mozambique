541f93e9a52f54a9b264ec569af50765ac16aef0
nym1k <sleeping.stars@gmail.com>
2022-05-19 18:49:42 +0100

    feat: two logos in header

--- content/themes/gef-mozambique/template-parts/header/logo.php
+++ content/themes/gef-mozambique/template-parts/header/logo.php
@@ -1,5 +1,26 @@
 <h1 class="header__title"><?php wp_title( '|', true, 'right' ); ?><?php bloginfo( 'name' ); ?></h1>
-<a class="header-logo" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo get_bloginfo('name'); ?>">
-  <span class="utility__screen-reader-only"><?php wp_title( '|', true, 'right' ); ?><?php bloginfo( 'name' ); ?></span>
-  <?php get_template_part( 'template-parts/global/logo', 'main' ); ?>
-</a>
+<div class="header-logos">
+  <ul class="header-logos__items">
+    <li class="header-logos__item">
+      <a class="header-logo" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo get_bloginfo('name'); ?>">
+        <span class="utility__screen-reader-only"><?php wp_title( '|', true, 'right' ); ?><?php bloginfo( 'name' ); ?></span>
+        <?php get_template_part( 'template-parts/global/logo', 'main' ); ?>
+      </a>
+    </li>
+
+    <li class="header-logos__item">
+      <a
+        href="https://www.mta.gov.mz"
+        target="_blank"
+        title="Ministério da Terra e Ambiente"
+        class="header-logo"
+      >
+        <img
+          src="<?php echo get_stylesheet_directory_uri() . '/dist/img/Mozambique-logo.png'; ?>"
+          alt="Ministério da Terra e Ambiente logo"
+          class="header-logo__logo header-logo__logo--mozambique"
+        >
+      </a>
+    </li>
+  </ul>
+</div>
